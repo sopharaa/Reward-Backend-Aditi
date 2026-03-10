1c566e4a79cb4025363993a83edf0e11b2e6d4bc
sopharaa <schhay2@paragoniu.edu.kh>
2026-03-10 12:07:04 +0700

    allow cors real frontend domain

--- src/main/java/com/phara/pontrix_backend/config/SecurityConfig.java
+++ src/main/java/com/phara/pontrix_backend/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://reward-frontend-aditi.vercel.app/", "https://pointrix-frontend-bt96d.ondigitalocean.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000","https://reward-frontend-aditi.vercel.app/", "https://pointrix-frontend-bt96d.ondigitalocean.app", "https://pointrix.paragoniu.me"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
